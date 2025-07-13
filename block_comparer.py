72f14f228a95fa3b6fcfc07a38479e45c48976a2
afif004 <afifdidar03@gmail.com>
2025-07-13 11:43:30 +0600

    updated iteration range in lcs

--- block_comparer.py
+++ block_comparer.py
@@ -2,22 +2,23 @@ def lcs(X, Y):
     m, n = len(X), len(Y)
     dp = [[0] * (n + 1) for _ in range(m+1)]
 
-    for i in range(m):
-        for j in range(n):
-            if X[i] == Y[j]:
-                dp[i + 1][j + 1] = dp[i][j] + 1
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if X[i-1] == Y[j-1]:
+                dp[i][j] = dp[i-1][j-1] + 1
             else:
-                dp[i + 1][j + 1] = max(dp[i + 1][j], dp[i][j + 1])
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1])
 
-    i, j = m, n
     lcs_seq = []
+    i, j = m, n
     while i > 0 and j > 0:
-        if X[i - 1] == Y[j - 1]:
-            lcs_seq.append(X[i - 1])
-            i = i - 1
-            j = j - 1
-        elif dp[i - 1][j] > dp[i][j - 1]:
-            i = i - 1
+        if X[i-1] == Y[j-1]:
+            lcs_seq.append(X[i-1])
+            i -= 1
+            j -= 1
+        elif dp[i-1][j] > dp[i][j-1]:
+            i -= 1
         else:
-            j = j - 1
+            j -= 1
+            
     return dp[m][n], lcs_seq[::-1]
